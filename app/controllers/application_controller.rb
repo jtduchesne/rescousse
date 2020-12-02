29b0afd8a8ce952652d7cab22a393c1b41aaf7bf
jtduchesne <jtduchesne@gmail.com>
2020-12-02 04:39:56 -0500

    Implement setting the Locale from the URL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+  
+  def default_url_options
+    { locale: I18n.locale.to_s.presence }
+  end
+  
+private
+  def set_locale
+    if locale = params[:locale].presence
+      I18n.locale = locale
+      
+      if locale == preferred_language
+        params[:locale] = nil
+      end
+    else
+      I18n.locale = preferred_language || I18n.default_locale
+    end
+  end
+  
+  def preferred_language
+    request.headers['HTTP_ACCEPT_LANGUAGE'] &&
+    request.headers['HTTP_ACCEPT_LANGUAGE'].scan(/fr|en/i).first
+  end
 end
