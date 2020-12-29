6396b5b9d94ab916f67e5236012f2c12394b3d8d
jtduchesne <jtduchesne@gmail.com>
2020-12-29 14:11:52 -0500

    Fix: Filter some gems from RSpec’s backtrace
    Namely “spring”, “webmock” and “bootsnap”

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,5 +61,5 @@ RSpec.configure do |config|
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
-  config.filter_gems_from_backtrace("factory_bot")
+  config.filter_gems_from_backtrace("factory_bot", "spring", "webmock", "bootsnap")
 end
